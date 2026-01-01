cbbfa0abbcbe8be69efd8b04448366b3fe3750a3
sudo-suadKF <suadfehric@outlook.com>
2026-01-01 23:11:52 +0100

    completed action

---
+++ index.js
@@ -0,0 +1,9 @@
+const core = require('actions/core')
+
+try {
+    const NameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello, ${NameToGreet}!`);
+    core.setOutput('time', new Date().toTimeString());
+} catch (error) {
+    core.setFailed(error.message);
+}
